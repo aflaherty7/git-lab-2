f0fcf9b44e2e60ed103162dbbc59e0e80423903f
Aidan Flaherty <114607858+aflaherty7@users.noreply.github.com>
2023-01-25 10:21:13 -0500

    Added git-lab-program file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aidan Flaherty
+ *        Date: jan 24th 2023
+ * Description: cpp code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0; 
+}// main
